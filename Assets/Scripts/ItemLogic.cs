692f9fe032c2c970cee0fa6a9d241777e8b7634a
informaticaesne <74909585+alonsogg99@users.noreply.github.com>
2024-03-20 18:21:40 +0100

    first commit

---
+++ Assets/Scripts/ItemLogic.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts.DataStructures;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ItemLogic : MonoBehaviour
+    {
+
+        public string Tag {get { return PlaceableItem!=null?PlaceableItem.Tag:""; } }
+        
+        public Sprite ActiveSprite;
+
+        public PlaceableItem.ItemType Type;
+        
+        public PlaceableItem PlaceableItem { get; set; }
+        
+        public string RequieredTags
+        {
+            get
+            {
+                var str = "Reqs: ";
+                return PlaceableItem.Preconditions.Aggregate(str,(curr, next) => curr + ", " + next.Tag);
+
+            }
+        }
+
+        public void Start()
+        {
+            Debug.Log(Tag+ ">"+ RequieredTags);
+            
+        }
+        void OnTriggerEnter2D(Collider2D collider2D)
+
+        {
+            if (!collider2D.gameObject.CompareTag("Player")) return;
+            if (PlaceableItem.Preconditions.Any(o => !o.Activated))
+            {
+                return;
+            }
+            var render = GetComponent<SpriteRenderer>();
+            render.sprite = ActiveSprite;
+            PlaceableItem.Activated = true;
+
+
+            if (this.Type == PlaceableItem.ItemType.Goal && GameManager.instance.ActiveEnemies.Count == 0)
+                QuitGame();
+            if (this.Type == PlaceableItem.ItemType.Enemy)
+            {
+                GameManager.instance.ActiveEnemies.Remove(gameObject);
+                GameObject.Destroy(gameObject, 0.5f);
+            }
+                
+        }
+
+        public void QuitGame()
+        {
+            // save any game data here
+#if UNITY_EDITOR
+            // Application.Quit() does not work in the editor so
+            // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+        }
+        void OnGUI()
+        {
+            Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+            GUI.Label(new Rect(pos.x-25,Screen.height-pos.y,70,20), Tag );
+            GUI.Label(new Rect(pos.x - 25, Screen.height - pos.y - 20, 370, 20), RequieredTags);
+        }
+    }
+}
